5a5ffe92f8bb6247d7b230e629026ef1bc0db0f8
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-04-02 21:59:24 +0200

    some progress

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
-
+use number_factorization::Factorization;
 
 fn factorial(n: u32) -> number_factorization::Factorization {
     // n*(n-1)*...*i / ( k*(k-1)*...*1 > upper_bound ?
 
-    let mut f = number_factorization::get_factorization(2);
+    let mut f = number_factorization::get_one();
 
-    for i in 3..n + 1 {
+    for i in 2..n + 1 {
         let f1 = number_factorization::get_factorization(i);
         f = number_factorization::multiply(f, f1);
     }
 
-    return f;
+    f
 }
 
 fn get_n_over_r(n: u32, r: u32) -> u128 {
-    
     let a = factorial(n);
-    let b = number_factorization::multiply( factorial(r), factorial(n-r));
+    let b = number_factorization::multiply(factorial(r), factorial(n - r));
 
-    let c = number_factorization::division( a, b );
+    let c = number_factorization::division(a, b);
 
     let r = number_factorization::get_number(c);
 
     return r;
-
 }
 
 fn main() {
