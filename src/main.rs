ca90e21617f96b412155bd3d8c48ca128b937a1a
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-04-02 21:53:47 +0200

    some progress

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,45 @@
-fn get_shortened_factorial( i: u32, j:u32  ) -> u128 {
 
-    assert!( j<= i); 
-    let mut acc:u128 = j as u128; 
 
-    for k in j+1..i+1 { 
-        acc *= k as u128;
+fn factorial(n: u32) -> number_factorization::Factorization {
+    // n*(n-1)*...*i / ( k*(k-1)*...*1 > upper_bound ?
+
+    let mut f = number_factorization::get_factorization(2);
+
+    for i in 3..n + 1 {
+        let f1 = number_factorization::get_factorization(i);
+        f = number_factorization::multiply(f, f1);
     }
 
-    acc
-
-
+    return f;
 }
 
+fn get_n_over_r(n: u32, r: u32) -> u128 {
+    
+    let a = factorial(n);
+    let b = number_factorization::multiply( factorial(r), factorial(n-r));
 
+    let c = number_factorization::division( a, b );
 
-fn factorial( n: u32 ) -> number_factorization::Factorization {
-   
-   // n*(n-1)*...*i / ( k*(k-1)*...*1 > upper_bound ?
+    let r = number_factorization::get_number(c);
 
-   let mut f : number_factorization::Factorization; 
-
-   for i in 0..n+1 {
-
-    let f1 = number_factorization::get_factorization(i);
-    f = number_factorization::multiply( f, f1 );
-
-   }
-
-
-}
-
-
-fn get_n_over_r( n: u32, r: u32 ) -> u128 { 
-
-    let mut hi = r; 
-    let mut low =  n-r;
-
-    if hi<low {
-        let temp = hi; 
-        hi = low; 
-        low = temp;   
-    } 
-
-
-
-    get_shortened_factorial(n, hi+1) / get_shortened_factorial( low, 1 ) 
+    return r;
 
 }
 
 fn main() {
-
-    assert!( get_shortened_factorial(3, 1) == 6 );
-    assert!( get_shortened_factorial(23, 22) == 23*22 );
-    assert!( get_n_over_r(5, 3)==10);
-    assert!( get_n_over_r(23, 10) == 1144066 );
+    assert!(1 == number_factorization::get_number(factorial(1)));
+    assert!(2 == number_factorization::get_number(factorial(2)));
+    assert!(6 == number_factorization::get_number(factorial(3)));
+    assert!(get_n_over_r(23, 10) == 1144066);
     println!("Hello, world!");
 
-let mut k=0; 
-    for n in 1..101 { 
-        for r in 1..n { 
-            if get_n_over_r(n,r )> 1000000 { 
-k+=1;
+    let mut k = 0;
+    for n in 1..101 {
+        for r in 1..n {
+            if get_n_over_r(n, r) > 1000000 {
+                k += 1;
             }
         }
     }
-    println!( "answer: {}", k);
+    println!("answer: {}", k);
 }
